8dd4d4a243232b70d12220a9f0a638829ca4a7c6
owner <owner@ThinkBoi>
2020-09-24 19:39:22 -0400

    Created Course class

---
+++ back-end/macademia/Course.java
@@ -0,0 +1,50 @@
+package macademia;
+import java.util.List;
+
+/*
+ * Contains information for a Course
+ * @author Kurcell
+ */
+
+public class Course {
+	
+	//-[Fields]----------------------------------------------
+	
+	private String name;
+	private String dept;
+	private int code;
+	private List<Course> prereq;
+	
+	//-[Constructor]-----------------------------------------
+	
+	public Course(String name, String dept, int code, List<Course> prereq) {
+		this.name = name;
+		this.dept = dept;
+		this.code = code;
+		this.prereq = prereq;
+	}
+	
+	//-[Getters]---------------------------------------------
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getDept() {
+		return this.dept;
+	}
+	public int getCode() {
+		return this.code;
+	}
+	public List<Course> getPrereq(){
+		return this.prereq;
+	}
+	
+	//-[Methods]--------------------------------------------
+	
+	public void addPrereq(Course course) {
+		this.prereq.add(course);
+	}
+	
+
+}
