39ad3aa7352495c58a5e5b7f9b4bd262b8c92937
Giovanni <giovanni.garcia2@upr.edu>
2020-09-26 17:12:31 -0400

    created section class

---
+++ back-end/macademia/backend/auth/Section.java
@@ -0,0 +1,66 @@
+package macademia.backend.auth;
+
+/**
+ * Holds a Class Section in Macademia
+ * @author Giovanni Garcia
+ */
+public class Section {
+    //Variables
+    private String secNum;
+    private String day;
+    private int time;
+   //private Class class;
+
+    //Constructor
+    public Section(String secNum, String day, int time){
+        this.secNum = secNum;
+        this.day = day;
+        this.time = time;
+        //this.class = class;
+    }
+
+    /**
+     * Sets the section Number of this object
+     * @return N/A
+     */
+    public void setSecNum(String s) { this.secNum = s;}
+
+    /**
+     * Sets the day of this object
+     * @return N/A
+     */
+    public void setDay(String d) {this.day = d;}
+
+    /**
+     * Sets the time of this object
+     * @return N/A
+     */
+    public void setTime(int t) {this.time = t;}
+
+    /**
+     * Gets the section number of this object
+     * @return secNum
+     */
+    public String getSecNum() {return this.secNum;}
+
+    /**
+     * Gets the day of this object
+     * @return day
+     */
+    public String getDay() {return this.day;}
+    
+    /**
+     * Gets the time of this object
+     * @return time
+     */
+    public int getTime() {return this.time;}
+
+    /**
+     * Gets the parent object of this object
+     * @return class
+     */
+   //public Class getClass(){return this.class} ;
+
+    
+
+}
