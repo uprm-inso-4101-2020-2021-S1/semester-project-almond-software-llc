745c2cf73eef81cc188447d50db50c5e73622b50
jose.bermudez8 <jose.bermudez8@upr.edu>
2020-09-27 20:40:11 -0400

    Created Matricula imported courses

--- back-end/macademia/Course.java
+++ back-end/macademia/Course.java
@@ -15,32 +15,60 @@ public class Course {
 	private String name;
 	private String dept;
 	private int code;
+	private int credits;
 	private List<Course> prereq;
 	private List<Section> sections;
 	
 	//-[Constructor]-----------------------------------------
 	
-	public Course(String name, String dept, int code, List<Course> prereq) {
+	public Course(String name, String dept, int code, int credits, List<Course> prereq) {
 		this.name = name;
 		this.dept = dept;
 		this.code = code;
+		this.credits = credits;
 		this.prereq = prereq;
 	}
 	
 	//-[Getters]---------------------------------------------
-	
+
 	public String getName() {
-		return this.name;
+		return name;
 	}
-	
 	public String getDept() {
-		return this.dept;
+		return dept;
 	}
 	public int getCode() {
-		return this.code;
+		return code;
 	}
-	public List<Course> getPrereq(){
-		return this.prereq;
+	public int getCredits() {
+		return credits;
+	}
+	public List<Course> getPrereq() {
+		return prereq;
+	}
+	public List<Section> getSections() {
+		return sections;
+	}
+	
+	//-[Setters]---------------------------------------------
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
+	public void setPrereq(List<Course> prereq) {
+		this.prereq = prereq;
+	}
+	public void setSections(List<Section> sections) {
+		this.sections = sections;
 	}
 	
 	//-[Methods]--------------------------------------------
@@ -48,6 +76,4 @@ public class Course {
 	public void addPrereq(Course course) {
 		this.prereq.add(course);
 	}
-	
-
 }
---
+++ back-end/macademia/Matricula.java
@@ -0,0 +1,39 @@
+package macademia;
+import java.util.List;
+import macademia.Course;
+
+public class Matricula {
+	
+	//-[Fields]----------------------------------------------
+	private List<Course> courses;
+	private int totalCredits; 
+	private String period; 
+
+	//-[Constructor]-----------------------------------------
+	public Matricula(List<Course> courses, int totalCredits, String period) {
+		this.courses = courses;
+		this.totalCredits = totalCredits;
+		this.period = period; 		
+	}
+	
+	//-[Getters]---------------------------------------------
+	public List<Course> getCourses() {
+		return courses;
+	}
+	public int getTotalCredits() {
+		return totalCredits;
+	}
+	public String getPeriod() {
+		return period;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+	public void setTotalCredits(int totalCredits) {
+		this.totalCredits = totalCredits;
+	}
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+}
