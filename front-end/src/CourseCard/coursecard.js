d8ed6eb58d8dcfab600d913917a6809087d8158d
Kurcell <kevnpurcell@gmail.com>
2020-10-11 14:37:53 -0400

    Implemented design for Course Card

--- front-end/src/CourseCard/coursecard.js
+++ front-end/src/CourseCard/coursecard.js
@@ -2,14 +2,39 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+import { Table } from '@material-ui/core';
 
 export default function CourseCard() {
 
     return (
-        <Card style={{ width: '150px', height: '50px' }}>
-            <CardContent>
-                <Typography>CURSO 4200</Typography>
+        <Card variant="outlined" style={{width: '450px', height: '150px', borderColor: '#707070', fontFamily: 'Times New Roman'}}>
+            <div style={{display: "flex"}}>
+            <div style={{width: 410}}>
+            <CardContent style={{}}>
+                <Typography>
+                    <Table>
+                        <td>
+                            <span style={{fontSize: 32, fontWeight: "bold"}}>CIIC3001</span>
+                            <span style={{fontSize: 20, color: '#7f7f7f', position:"relative", bottom: 4}}> - 001</span>
+                            <br></br>
+                            <span style={{fontSize: 22, color: '#7f7f7f'}}>Mandatory Class 1</span>
+                            <br></br>
+                            <span style={{fontSize: 24, color: '#7f7f7f'}}>3 credits</span>
+                        </td>
+                        <td >
+                            <IconButton aria-label="delete" style={{positon:"relative", bottom: 20, left: 40}}>
+                                <ErrorOutlineIcon />
+                            </IconButton>
+                        </td>
+                    </Table>
+                </Typography>
             </CardContent>
+            </div>
+            <div style={{backgroundColor: '#707070', width: 2}}></div>
+            <div style={{backgroundColor: '#71a772', width: 38}}></div>
+            </div>
         </Card>
     );
 }
