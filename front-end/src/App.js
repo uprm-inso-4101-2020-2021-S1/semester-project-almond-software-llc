a4db8640ce5bc9b4adb3827580e5df005ee2fa4d
Natalia Lopez <natalia.lopez17@upr.edu>
2020-10-17 22:31:43 -0400

    redid header and drawer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,57 +1,12 @@
 import React from 'react';
-import Header from './Header/header';
-import Matricula from './Matricula/matricula';
-import CourseList from './CourseList/courselist'
-import TempHeader from './Header/temp-header';
-import { fade, makeStyles } from '@material-ui/core/styles';
+import Header from './Header/header-drawer.js';
 import './App.css';
-import Drawer from "./Header/drawer.js";
-
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
-  // necessary for content to be below app bar
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
 
 function App() {
-
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-
+    <div className="root">
       <Header />
-      {/* <Drawer/> */}
-
-      {/* <nav className={classes.drawer} aria-label="mailbox folders">
-        <CourseList />
-      </nav> */}
-
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <Matricula />
-      </main>
-
     </div>
   );
 }
-
 export default App;
--- front-end/src/CourseCard/coursecard.js
+++ front-end/src/CourseCard/coursecard.js
@@ -4,30 +4,32 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
-import { Table } from '@material-ui/core';
+import Table from "@material-ui/core/Table";
+import TableRow from "@material-ui/core/TableRow";
+
 
 export default function CourseCard() {
 
     return (
-        <Card variant="outlined" style={{width: '200px', height: '75px', borderColor: '#707070', fontFamily: 'Times New Roman'}}>
+        <Card variant="outlined" style={{width: '200px', height: '90px', borderColor: '#707070', fontFamily: 'Times New Roman'}}>
             <div style={{display: "flex"}}>
             <div style={{width: 410}}>
             <CardContent >
                 <Typography>
                     <Table>
-                        <td style={{position: "relative", bottom: 10, right: 5, lineHeight: 1}}>
+                        <TableRow style={{position: "relative", bottom: 10, right: 5, lineHeight: 1}}>
                             <span style={{fontSize: 18, fontWeight: "bold"}}>CIIC3001</span>
                             <span style={{fontSize: 10, color: '#7f7f7f', position:"relative", bottom: 4}}> - 001</span>
                             <br></br>
                             <span style={{fontSize: 12, color: '#7f7f7f'}}>Mandatory Class 1</span>
                             <br></br>
                             <span style={{fontSize: 14, color: '#7f7f7f'}}>3 credits</span>
-                        </td>
-                        <td >
-                            <IconButton aria-label="delete" style={{positon:"relative", width: 10, height: 10, bottom: 20, left: 20}}>
+                        </TableRow>
+                        <TableRow >
+                            <IconButton aria-label="delete" style={{positon:"relative", width: 10, height: 10, bottom: 25, left: 80}}>
                                 <ErrorOutlineIcon style={{width: 15, height: 15}}/>
                             </IconButton>
-                        </td>
+                        </TableRow>
                     </Table>
                 </Typography>
             </CardContent>
