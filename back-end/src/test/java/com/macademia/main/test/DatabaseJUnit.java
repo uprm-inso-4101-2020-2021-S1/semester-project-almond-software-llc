28238accf7a85044f2d84551452c2df34cc214f9
Ignacio Tampe <Igtampe@outlook.com>
2020-10-27 20:33:33 -0400

    Import Cleanup

--- back-end/src/test/java/com/macademia/main/test/DatabaseJUnit.java
+++ back-end/src/test/java/com/macademia/main/test/DatabaseJUnit.java
@@ -2,8 +2,6 @@ package com.macademia.main.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Order;
