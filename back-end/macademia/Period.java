b66646739a96dcb3d571a2569360d329e39b28f9
JosueRamirez1997 <josue.ramirez3@upr.edu>
2020-11-02 17:51:42 -0400

    My work...

---
+++ back-end/macademia/Period.java
@@ -0,0 +1,31 @@
+package macademia;
+
+public class Period {
+    private int start;
+    private int end;
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public void setEnd(int end) {
+        this.start = end;
+    }
+
+    public Period(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public boolean Conflict(Period sec){
+        return !(this.end <sec.start||this.start>sec.end);
+    }
+}
--- back-end/macademia/Section.java
+++ back-end/macademia/Section.java
@@ -3,6 +3,8 @@ package macademia;
 /**
  * Holds a Class Section in Macademia
  * @author Giovanni Garcia
+ * Period additions
+ * @author Josue Ramirez
  */
 public class Section {
     //Variables
@@ -10,6 +12,7 @@ public class Section {
     private String day;
     private String time;
     private Course course;
+    private Period period;
 
     //Constructor
     public Section(String secNum, String day, String time, Course course){
@@ -20,7 +23,31 @@ public class Section {
         
         //Link this Section's Course to this section
         course.addSection(this);
-        
+
+        this.period= timetoPeriod(time);
+    }
+
+    public void setPeriod(Period period) {
+        this.period = period;
+    }
+
+    public Period getPeriod() {
+        return period;
+    }
+
+    private Period timetoPeriod(String time) {
+        String[] per= time.split("-");
+        char temp = per[0].charAt(per[0].indexOf('M')-1);
+        String[] re=per[0].split(":");
+        per[0]=re[0]+re[1].substring(0,re[1].indexOf('M')-1);
+        int start=Integer.parseInt(per[0]);
+        if(temp=='P')start+=1200;
+        temp = per[1].charAt(per[1].indexOf('M')-1);
+        re=per[1].split(":");
+        per[1]=re[0]+re[1].substring(0,re[1].indexOf('M')-1);
+        int end=Integer.parseInt(per[1]);
+        if(temp=='P')end+=1200;
+        return new Period(start,end);
     }
 
     /**
@@ -72,6 +99,15 @@ public class Section {
 	 * @return DeptShortNameCourseCode-SectionNum on Days during Time (IE "DRAM3001-21 on Tuesday, Thursday during 5:30-7:00")
     */
    public String toString() {return getCourse().getDept().getShortName() + getCourse().getCode() + "-" + getSecNum() + " on " + getDay() + " during " + getTime();}
-    
+    /*
+    Check if there is conflict in the sections in the matricula
+     */
+   public boolean Conflict(Section sec){
+       for (int i = 0; i < sec.day.length(); i++)
+           for(int j = 0; j < this.day.length(); j++)
+               if(this.day.charAt(j)==sec.day.charAt(i))
+                   return this.period.Conflict(sec.period);
+       return false;
+   }
 
 }
